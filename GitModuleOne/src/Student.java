e39e22fee87a590f8c49009f435f1e4d3a6e1020
LegendsNeverDie1219 <1571710215@qq.com>
2021-07-25 14:06:11 +0800

    添加了studnet.java teacher.java以及 .gitignore

--- GitModuleOne/src/Student.java
+++ GitModuleOne/src/Student.java
@@ -5,4 +5,8 @@
  * @date 2021/7/25 12:18
  */
 public class Student {
+    private Integer  studentNumber;
+    private String name;
+    private String sex;
+    private Integer age;
 }
--- GitModuleTwo/src/Teacher.java
+++ GitModuleTwo/src/Teacher.java
@@ -5,4 +5,8 @@
  * @date 2021/7/25 13:07
  */
 public class Teacher {
+    private Integer  jobNumber;
+    private String name;
+    private String sex;
+    private Integer age;
 }
